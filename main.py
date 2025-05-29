c1eda8ab6c896a339d9e1557417430809ff75991
sharlaa <121415756+sharlaa@users.noreply.github.com>
2025-05-29 19:16:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1)
 
 # Webcam capture
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 print("📸 Align your face. Press SPACE to capture.")
 
 user_avg = None
